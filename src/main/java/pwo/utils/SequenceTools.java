074fd4b0a46edb83b7c399d8c71fb9629ea0adb9
student <student@localhost>
2023-11-07 17:02:32 +0100

    Dystrybucja 1 - optymalizacja wstecz

--- src/main/java/pwo/utils/SequenceTools.java
+++ src/main/java/pwo/utils/SequenceTools.java
@@ -1,42 +1,57 @@
- package pwo.utils;
+package pwo.utils;
 
- import java.io.FileWriter;
- import java.io.BufferedWriter;
- import java.io.IOException;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
 
- public class SequenceTools {
+public class SequenceTools {
 
- private static String getTerms(SequenceGenerator sg,
- int from, int to, String sep) {
+    private static String getTerms(SequenceGenerator sg,
+            int from, int to, String sep) {
 
- int i = from, stop = to, step = from > to ? -1 : 1;
- String terms = "";
+        int i = from, stop = to, step = 1;
+        String terms = "";
+        if (from < to) {
+            while (true) {
+                terms += sg.getTerm(i) + sep;
+                if (i == stop) {
+                    return terms.trim();
+                }
+                i += step;
+            }
+        } else {
+            i = to;
+            stop = from;
+            while (true) {
+                terms = sg.getTerm(i) + sep + terms;
+                if (i == stop) {
+                    return terms.trim();
+                }
+                i += step;
+            }
+        }
+    }
 
- while(true){
- terms += sg.getTerm(i) + sep;
- if(i==stop) return terms.trim();
- i+=step;
- }
- }
+    public static String getTermsAsColumn(SequenceGenerator sg,
+            int from, int to) {
+        return getTerms(sg, from, to, "\n");
+    }
 
- public static String getTermsAsColumn(SequenceGenerator sg,
- int from, int to) {
- return getTerms(sg, from, to, "\n");
- }
+    public static String getTermsAsLine(SequenceGenerator sg,
+            int from, int to) {
+        return getTerms(sg, from, to, " ");
+    }
 
- public static String getTermsAsLine(SequenceGenerator sg,
- int from, int to) {
- return getTerms(sg, from, to, " ");
- }
+    public static boolean writeToFile(SequenceGenerator sg,
+            int from, int to, String fileName) {
 
- public static boolean writeToFile(SequenceGenerator sg,
- int from, int to, String fileName) {
+        try (BufferedWriter writer = new BufferedWriter(
+                new FileWriter(fileName))) {
+            writer.write(getTermsAsColumn(sg, from, to));
+        } catch (IOException ex) {
+            return false;
+        }
 
- try ( BufferedWriter writer = new BufferedWriter(
- new FileWriter(fileName))) {
- writer.write(getTermsAsColumn(sg, from, to));
- } catch (IOException ex) { return false; }
-
- return true;
- }
- }
+        return true;
+    }
+}
